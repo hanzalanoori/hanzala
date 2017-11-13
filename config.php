a31d8c899e1948b49b5e0bc4865940de8f494300
Hanzala1 <Hanzalanoori616@gmail.com>
2017-11-13 12:48:16 +0500

    added Signed-off-by: Hanzala1 <Hanzalanoori616@gmail.com>

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ define("_TIMEZONE", "Asia/Karachi");
 define("_FALSE", "0");
 define("_TRUE", "1");
 
-define("_WEBADDR","http://localhost/");
+define("_WEBADDR","http://192.168.0.1/");
 /**************CONNECTION****************/
 
 ?>
